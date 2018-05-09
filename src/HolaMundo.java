c2c337c8523008917966d09da6b3b73e2787da2c
DD <DD@PC-D>
2018-05-09 13:41:51 +0200

    PRUEBA DE CONFLICTO

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -7,7 +7,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo java");
-		System.out.println("Adios Mundo java");
+		System.out.println("Adios Mundo java ");
+		System.out.println("PRUEBA DE CONFLICOS");
 	}
 
 }
