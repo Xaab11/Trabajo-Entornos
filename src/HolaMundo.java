1ce2bc632fafacef2925a7e6c009303c8db8a126
David <David@AFEQUIPO12>
2018-05-10 10:32:49 +0200

    Commit añadiendo el syso PRUEBA DE CONFLICTO

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -8,7 +8,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo java");
 		System.out.println("Adios Mundo java");
-		
+		System.out.println("PRUEBA DE CONFLICTO");
+
 
 		
 		
